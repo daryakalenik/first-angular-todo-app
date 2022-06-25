6dba1637d6a5ae1e2639f76031393e4020f1e36e
Darya Kalenik <kalenikdarya.dev@gmail.com>
2022-06-25 19:57:52 +0300

    created todo-app with editing and deleting using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  text: string;
+  isCompleted: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todos: Todo[] = [];
+  todoItem = '';
+  todoToEdit = { text: '', isCompleted: false };
+  addTodo() {
+    this.todoItem
+      ? (this.todos.push({ text: this.todoItem, isCompleted: false }),
+        (this.todoItem = ''))
+      : null;
+  }
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((item, index) => {
+      return index !== id;
+    });
+    console.log(this.todos);
+  }
+  markCompleted(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+  editTodo(id: number) {
+    this.todoToEdit = this.todos[id];
+  }
+  addEditedTodo(id: number) {
+    this.todos[id] = this.todoToEdit;
+    this.todoToEdit = { text: '', isCompleted: false };
+  }
 }
